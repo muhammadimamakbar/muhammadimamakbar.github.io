cfe186baf18cff3b67a5c5f3de1d4a69b02f1395
muhammadimamakbar <recovery4system@gmail.com>
2024-05-04 05:01:20 +0700

    Initial commit

---
+++ tls-website/script.js
@@ -0,0 +1,8 @@
+red1 = document.getElementById("red-1");
+red2 = document.getElementById("red-2");
+green1 = document.getElementById("green-1");
+green2 = document.getElementById("green-2");
+
+while (true) {
+    red1.style.backgroundColor = "red";
+}
